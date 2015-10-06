00eb8f9e0470bb99571d58ff444e9c043ba6512a
Johnny <jchoo157@gmail.com>
2015-10-05 22:55:56 -0400

    added several fixes to views

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -23,6 +23,7 @@ class PinsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'pins#index'
   devise_for :users
+
   resources :pins do
     member do
       put 'like', to: 'pins#upvote'
+      put 'unlike', to: 'pins#downvote'
     end
   end
 end
